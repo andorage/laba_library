b00d0019ba06658adbbb1b8287bf853123cd8b89
Егор Андрианов <andrianov_egr@mail.ru>
2025-01-09 10:39:40 +0300

    Добавлен исходный код библиотеки со второй лабы

---
+++ InterestCalculatorLibrary/InterestCalculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterestCalculatorLibrary
+{
+    public class InterestCalculator
+    {
+        // Метод для расчета простых процентов
+        public double SimpleInterest(double principal, double rate, int periods)
+        {
+            return principal * (1 + rate * periods);
+        }
+
+        // Метод для расчета сложных процентов
+        public double CompoundInterest(double principal, double rate, int periods)
+        {
+            return principal * Math.Pow(1 + rate, periods);
+        }
+
+        // Метод для расчета номинальной процентной ставки
+        public double NominalInterest(double principal, double rate, int periods, int compoundsPerYear)
+        {
+            return principal * Math.Pow(1 + rate / compoundsPerYear, compoundsPerYear * periods);
+        }
+
+        // Метод для расчета непрерывных процентов
+        public double ContinuouslyCompoundedInterest(double principal, double growthRate, int periods)
+        {
+            return principal * Math.Exp(growthRate * periods);
+        }
+
+        // Метод для расчета реальной процентной ставки
+        public double RealInterestRate(double nominalRate, double inflationRate)
+        {
+            return (nominalRate - inflationRate) / (1 + inflationRate);
+        }
+    }
+}
